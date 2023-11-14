8c911378318a38369cf8bd702a57d02f89c81717
Chrispus <skilochris@gmail.com>
2023-11-14 15:48:32 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,15 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Hey my name is chris</p>
+      <img src={image} alt="I made this"></img>
+
+    </div>
+  );
 }
 
 export default About;
